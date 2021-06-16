bee8720109915b7553fdc1f64fe3d34a8ea77405
Lucas Paiva <lucas.ramalhopaiva@hotmail.com>
2021-06-16 11:28:08 -0300

    Projeto Finalizado

---
+++ BankAccountException/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankAccountException.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+        }
+    }
+}
--- BankAccountException/Program.cs
+++ BankAccountException/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BankAccountException.Entities;
+using BankAccountException.Entities.Exceptions;
+using System;
+using System.Globalization;
 
 namespace BankAccountException
 {
@@ -6,7 +9,30 @@ namespace BankAccountException
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter account data");
+            Console.Write("Number: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Holder: ");
+            string holder = Console.ReadLine();
+            Console.Write("Initial balance: ");
+            double initialBalance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Withdraw limit: ");
+            double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+
+            Account account = new Account(n, holder, initialBalance, withdrawLimit);
+
+            try
+            {
+                Console.Write("Enter amount for withdraw: ");
+                double amount = double.Parse(Console.ReadLine());
+                account.Withdraw(amount);
+                Console.WriteLine("New Balance: " + account.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            catch(DomainException e)
+            {
+                Console.WriteLine("Error when withdrawing: " + e.Message);
+            }
         }
     }
 }
