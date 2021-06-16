6fb663a58aaa78ceada8a40b1802f247f1fc602b
Lucas Paiva <lucas.ramalhopaiva@hotmail.com>
2021-06-16 10:14:49 -0300

    Projeto iniciado

---
+++ BankAccountException/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankAccountException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
